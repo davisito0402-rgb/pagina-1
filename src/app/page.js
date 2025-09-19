1ddf49121ad2a255d8ef36fb07fda04a19cf4aff
david_henry <davisito0402@gmail.com>
2025-09-19 18:38:27 -0500

    actu de banner con favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Para Alguien especial<3',
+  description: 'Un detalle especial en septiembre ✨',
+};
+
 import AdvancedWinePage from "./AdvancedWinePage";
 
 export default function Page() {
